3ba23bb80164c09aff4ec36dacbe31aab512bfb8
Mark Dorrill <mark.dorrill@gmail.com>
2018-03-20 15:38:55 +0100

    Create verify-hugo-resources.bash

---
+++ verify-hugo-resources.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Script Name: verify-hugo-resources.bash
+#
+# Author: Mark Dorrill
+# Date : 20.03.2018
+#
+# Description: The following script verifies that resources are valid hugo filesets
+#
+# Run Information: This script is run manually.
+#
+# Standard Output: Any output is sent to a file called output.log
+#
+# Error Log: Any errors associated with this script are sent to a file called errors.log
+#
+
