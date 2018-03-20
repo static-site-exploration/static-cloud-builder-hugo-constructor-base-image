849b647046850a2f069d814b881b94e801093a3f
Mark Dorrill <mark.dorrill@gmail.com>
2018-03-20 15:39:04 +0100

    Create download-resources.bash

---
+++ download-resources.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Script Name: download-resources.bash
+#
+# Author: Mark Dorrill
+# Date : 20.03.2018
+#
+# Description: The following script downloads resources from the locations provided in arguments
+#
+# Run Information: This script is run manually.
+#
+# Standard Output: Any output is sent to a file called output.log
+#
+# Error Log: Any errors associated with this script are sent to a file called errors.log
+#
