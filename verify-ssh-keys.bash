e3bf6b8716da9a9a676cc3fc2178641f6d27b6a8
Mark Dorrill <mark.dorrill@gmail.com>
2018-03-21 23:58:05 +0100

    Create verify-ssh-keys.bash

---
+++ verify-ssh-keys.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Script Name: verify-ssh-keys.bash
+#
+# Author: Mark Dorrill
+# Date : 20.03.2018
+#
+# Description: The following script downloads resources from the locations provided in arguments
+#
+# Run Information: This script is run manually.
+#
+# Standard Output: Any output is sent to a file called output.log
+#
+# Error Log: Any errors associated with this script are sent to a file called errors.log
+#
+
