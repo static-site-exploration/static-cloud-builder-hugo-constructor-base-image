be8df034b9e6a5b8e961c61cfc5899830a61d8d8
Mark Dorrill <mark.dorrill@gmail.com>
2018-03-22 01:09:30 +0100

    Update build.bash

--- build.bash
+++ build.bash
@@ -1,8 +1,27 @@
 
+#!/bin/bash
+#
+# Script Name: build.bash
+#
+# Author: Mark Dorrill
+# Date : 20.03.2018
+#
+# Description: The following script builds the hugo package using the loaded site configuation and content location supplied in arguments
+#
+# Run Information: This script is run manually.
+#
+# Standard Output: Any output is sent to a file called output.log
+#
+# Error Log: Any errors associated with this script are sent to a file called errors.log
+#
+
+set -e;
+
+echo "hello from build.bash, using: $@";
+
 # Build the package
 
 # Push the package to resource location (git, gs, file)
 
 # Deploy
 
-echo "hello from build.bash, using: $@";
