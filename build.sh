3357f706948b0d61cb70ae1c2b278698dd03bdf8
Mark Dorrill <mark.dorrill@gmail.com>
2018-03-22 16:28:08 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 
 #!/bin/sh
 #
-# Script Name: build.bash
+# Script Name: build.sh
 #
 # Author: Mark Dorrill
 # Date : 20.03.2018
 #
-# Description: The following script builds the hugo package using the loaded site configuation and content location supplied in arguments
+# Description: NOTE! this is sh and not bash. The following script builds the hugo package using the loaded site configuation and content location supplied in arguments
 #
 # Run Information: This script is run manually.
 #
@@ -16,8 +16,7 @@
 #
 
 set -e;
-
-echo "hello from build.bash, using: $@";
+echo "hello from build.sh, using: $@";
 
 # Build the package, if no content location is supplied then create a test mardown file usng cat EOF
 
