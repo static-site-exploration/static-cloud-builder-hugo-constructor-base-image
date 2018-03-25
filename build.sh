bedb445b92dea0d1d1c57c332ac3e256ac4fe8a4
Mark Dorrill <mark.dorrill@gmail.com>
2018-03-25 22:11:19 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -19,19 +19,59 @@ set -e
 
 # Test hugo works
 test hugo
-echo "running script in directory:"; pwd
-echo "running with arguments:"; echo "$@"
+echo "running script in directory:"
+pwd
+echo "running with arguments:"
+echo "$@"
 
 echo "Listing / (container root /)"
 ls -a / # requires busybox or installed utils
 
-#ls /bin
-
 if [ -d "/workspace" ] 
 then
-  echo "WORKSPACE TEST POSITIVE"
-  ls /workspace
-  echo "hello from inside container" > /workspace/hello.txt
+  run_sequence
 else 
   echo "WORKSPACE TEST NEGATIVE"
 fi
+
+run_sequence() {
+  echo "WORKSPACE TEST POSITIVE"
+  ls /workspace
+  echo "hello from inside container" > /workspace/hello.txt
+  
+  if [ -z "$@" ]
+  then
+    echo ""
+    echo "-------------------------"
+    echo " NO ARGUMENTS RECIEVED ! "
+    echo "-------------------------"
+    echo ""
+  else
+    echo ""
+    echo "------------------"
+    echo " Using arguments: "
+    echo "------------------"
+    echo ""
+    echo "$@"
+    echo ""
+    echo "......................................................"
+  fi
+  
+  echo ""
+  echo "-----------------------------"
+  echo " Using envionment variables: "
+  echo "-----------------------------"
+  echo ""
+  echo "container_package_dir: " $container_package_dir
+  echo "container_build_dir: " $container_build_dir
+  echo "site_dir: " $site_dir
+  echo "site_config_file: " $site_config_file
+  echo "themes_dir: " $themes_dir
+  echo "theme_dir_name: " $theme_dir_name
+  echo "content_path: " $content_path
+  echo "destination_path: " $destination_path
+  echo ""
+  echo "......................................................"
+  
+}
+
