b752b74a0bf73e0296ed180d44320b18ed56fa6a
Mark Dorrill <mark.dorrill@gmail.com>
2018-03-25 14:38:52 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,10 +16,50 @@
 #
 
 set -e;
-echo "hello from build.sh, using: $@";
 
+# Test hugo works
 hugo --help;
 
-# Build the package, if no content location is supplied then create a test mardown file usng cat EOF
+if [ -z "$@" ] then; 
+
+  echo "hello from build.sh, NO ARGUMENTS RECIEVED";
+
+else
+
+  echo "hello from build.sh, using arguments: $@";
+
+fi;
+
+# Setup the variables that can be easily overridden
+content_path="${container_package_dir}/${content_dir}";
+destination_path="${container_build_dir}"
+
+# if $1 or $2 are set then override some of the default values already set in the image
+if [ -n "$1" ] then; 
+
+  content_path="$1"
+
+elif [ -n "$2" ] then;  
+
+  destination_path="$2"
+
+fi
+
+echo "Running hugo using the following flags:";
+echo "--config ${container_package_dir}/${site_dir}/${site_config_file}";
+echo "--themesDir ${container_package_dir}/${themes_dir}";
+echo "--theme ${theme_dir_name}";
+echo "--contentDir ${content_path}";
+echo "--destination ${destination_path}";
+
+hugo \
+  --config ${container_package_dir}/${site_dir}/${site_config_file} \
+  --themesDir ${container_package_dir}/${themes_dir} \
+  --theme ${theme_dir_name} \
+  \
+  --contentDir ${content_path} \
+  --destination ${destination_path} \
+
+
 
 # Verify the build is complete
